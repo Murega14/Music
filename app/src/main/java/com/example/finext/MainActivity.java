debb18e623b300da11df2d87b694db41c5834c87
Bravin Murega <tedmurega@gmail.com>
2023-03-16 22:43:51 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/finext/MainActivity.java
+++ app/src/main/java/com/example/finext/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.finext;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+        //onclick listener
+        ImageButton btn = findViewById(R.id.accButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LogoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

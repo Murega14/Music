5a7a93c7aefb23a0af9e187799b85ba2f9859de2
murega14 <tedmurega@gmail.com>
2023-05-25 18:10:17 +0300

    added a resource

--- app/src/main/java/com/example/finext/MainActivity.java
+++ app/src/main/java/com/example/finext/MainActivity.java
@@ -4,25 +4,47 @@ package com.example.finext;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.finext.fragments.Dashboard;
+import com.example.finext.fragments.Expense;
+import  com.example.finext.fragments.Income;
+
 import com.google.firebase.FirebaseApp;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemReselectedListener {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
-        //onclick listener
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageButton btn = findViewById(R.id.accButton);
-        btn.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, MainActivity.class);
-            startActivity(intent);
-        });
+
+         bottomNavigatiionView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.SetSelectedItemId(R.id.dashboard);
+
 
     }
+
+    Dashboard dashboardFragment = new Dashboard();
+    Expense expenseFragment = new Expense();
+    Income incomeFragment = new Income();
+
+    @Override
+    public boolean onNavigationItemReselected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.income) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, incomeFragment).commit();
+            return true;
+        }
+        return false;
+    }
+
 }

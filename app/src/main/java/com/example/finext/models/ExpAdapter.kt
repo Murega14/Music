92002058d91936b037830d04ff939f394e332cf6
Murega14 <tedmurega@gmail.com>
2023-07-18 18:46:22 +0300

    added an expense view to fetch data and added an adapter class for recycle view

---
+++ app/src/main/java/com/example/finext/models/ExpAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.finext.models
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finext.R
+import com.example.finext.models.ExpenseModel
+
+class ExpAdapter(private val expList: ArrayList<ExpenseModel>) :
+    RecyclerView.Adapter<ExpAdapter.ViewHolder>() {
+
+    private lateinit var mListener: OnItemClickListener
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(clickListener: OnItemClickListener) {
+        mListener = clickListener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.exp_list_item, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentExp = expList[position]
+        holder.tvExpName.text = currentExp.label
+    }
+
+    override fun getItemCount(): Int {
+        return expList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvExpName: TextView = itemView.findViewById(R.id.tvExpName)
+
+        init {
+            itemView.setOnClickListener {
+                mListener.onItemClick(adapterPosition)
+            }
+        }
+    }
+}
